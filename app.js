fa5f0d232ff71add508544b88c3a598635ec6cdf
Alex Linden <alextl2004@gmail.com>
2022-05-31 16:10:00 -0700

    first html and submit func

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log("Let's get this party started!");
+"use strict";
+
+/**this takes the val from the input el on HTML and returns it */
+function getSearchVal() {
+  let searchTerm = $('#search-text').val();
+  console.log(searchTerm);
+  //return searchTerm;
+}
+
+/**On submit this function will use getSearchVal to send the input val to
+ * giphy api */
+function handleSubmit(evt){
+  evt.preventDefault();
+  getSearchVal()
+}
+
+$('.submit-form').on('submit', handleSubmit);
+
+//console.log("Let's get this party started!");
