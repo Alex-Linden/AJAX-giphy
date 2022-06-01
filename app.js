bf6e6e5594498b654ca34ffeea0df9e4f8ddaad5
Alex Linden <alextl2004@gmail.com>
2022-05-31 17:50:48 -0700

    grab url and append to div

--- app.js
+++ app.js
@@ -4,29 +4,39 @@
 function getSearchVal() {
   let searchTerm = $('#search-text').val();
   console.log(searchTerm);
-  //return searchTerm;
+  return searchTerm;
 }
 /** Ajax func */
 
-async function getMeme() {
-
-
+async function getMeme(word) {
   let responce = await axios.get(
     "http://api.giphy.com/v1/gifs/search",
-    {params: {"api_key":"MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym","q":"funny"}})
-  console.log(responce.data)
+    { params: { "api_key": "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym", "q": word } });
+  console.log("getMeme=", responce.data.data[0]['images']['original']['url']);
+  let newMeme = responce.data.data[0]['images']['original']['url'];
+    renderGiph(newMeme)
+}
+
+function renderGiph(newMeme) {
+  let image = $('<img>');
+  image.attr('src', newMeme);
+  $('.giphs').append(image);
 }
 
 /**On submit this function will use getSearchVal to send the input val to
  * giphy api */
-function handleSubmit(evt){
+function handleSubmit(evt) {
   evt.preventDefault();
-  //let word = getSearchVal()
-  getMeme()
-
-
+  let word = getSearchVal();
+  let newMeme = getMeme(word);
+  console.log("newMeme=", newMeme);
 }
 
 $('.submit-form').on('submit', handleSubmit);
 
 //console.log("Let's get this party started!");
+
+
+/* let image = $('<img>');
+image.attr('src', newMeme);
+$('.giphs').append(image); */
